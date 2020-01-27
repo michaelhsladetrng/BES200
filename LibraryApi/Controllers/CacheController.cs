fe2441345426f75caebe68970cf6b8a5d3e2dfcc
michaelhsladetrng <51245469+michaelhsladetrng@users.noreply.github.com>
2020-01-27 21:00:22 +0000

    End of day 1

---
+++ LibraryApi/Controllers/CacheController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Controllers
+{
+    public class CacheController : Controller
+    {
+
+        IDistributedCache Cache;
+
+        public CacheController(IDistributedCache cache )
+        {
+            Cache = cache;
+        }
+
+        [HttpGet("/time2")]
+        [ResponseCache(Duration =15, Location = ResponseCacheLocation.Client)]
+        public async Task<ActionResult<string>> GetTime2()
+        {
+            return Ok($"the Time is {DateTime.Now.ToLongTimeString()}");
+        }
+
+        [HttpGet("/time")]
+        public async Task<ActionResult<string>> GetTheTime()
+        {
+            var time = await Cache.GetAsync("time");
+            string newTime = null;
+            if (time == null )
+            {
+                newTime = DateTime.Now.ToLongTimeString();
+                var encodedTime = Encoding.UTF8.GetBytes(newTime);
+                var options = new DistributedCacheEntryOptions()
+                    .SetAbsoluteExpiration(DateTime.Now.AddSeconds(15));
+                await Cache.SetAsync("time", encodedTime, options);
+            }
+            else
+            {
+                newTime = Encoding.UTF8.GetString(time);
+            }
+
+            return Ok($"It is now {newTime}");
+        }
+    }
+}
--- LibraryApi/Startup.cs
+++ LibraryApi/Startup.cs
@@ -67,6 +67,10 @@ namespace LibraryApi
                 c.IncludeXmlComments(xmlPath);
             });
 
+            services.AddDistributedRedisCache(options => {
+                options.Configuration = Configuration.GetValue<string>("redisHost");  //  "localhost";  TODO: put this in appsettings.json
+            });
+
            
         }
 
